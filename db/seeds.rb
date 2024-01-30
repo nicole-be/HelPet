1e20286599d0ec3dcaa53fc830daf60f3186287f
Nicole Besel <nicole.besel@gmx.de>
2024-01-30 16:38:30 +0100

    updated pet show and new html

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-user = User.create!(first_name: "Anna", last_name: "Mueller", email: "user@example.com", password: "password", address: "london", role:"owner")
-Pet.create!(species: "Dog", description: "A loyal and friendly companion", name: "Buddy", user: user)
-Pet.create!(species: "Cat", description: "An independent and playful pet", name: "Whiskers", user: user)
-Pet.create!(species: "Bird", description: "A colorful and chirpy friend", name: "Sunny", user: user)
+# user = User.create!(first_name: "Anna", last_name: "Mueller", email: "user@example.com", password: "password", address: "london", role:"owner")
+# Pet.create!(species: "Dog", description: "A loyal and friendly companion", name: "Buddy", user: user)
+# Pet.create!(species: "Cat", description: "An independent and playful pet", name: "Whiskers", user: user)
+# Pet.create!(species: "Bird", description: "A colorful and chirpy friend", name: "Sunny", user: user)
