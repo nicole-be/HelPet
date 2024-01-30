e66c84c08c064cccbb95ddb239442cccbdc1196b
Justine Auguste <justine_auguste@hotmail.com>
2024-01-30 19:28:39 +0100

    homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   # root to: "pages#home"
+  # Justine: Adding list of pets as root page
+  root to: "pets#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
