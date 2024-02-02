5350f6683196e726eafbea0706053d49ea5e9e05
Jendrik Mallah <jm.orders+lewagon@outlook.com>
2024-02-02 15:17:29 +0100

    fixed some front-end shit

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -9,10 +9,10 @@ class Booking < ApplicationRecord
   validates :end_date, comparison: { greater_than_or_equal_to: :start_date }
 
   def total_price
-    (((end_date - start_date).to_i) * pet.price).truncate
+    (((end_date - start_date + 1).to_i) * pet.price).truncate
   end
 
   def total_days
-    ((end_date - start_date).to_i)
+    ((end_date - start_date + 1).to_i)
   end
 end
